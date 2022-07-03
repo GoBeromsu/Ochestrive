10eb3e5a23fc573d481e729fed543c5bca3ae788
Starlight258 <audwl03071@gmail.com>
2022-07-03 18:31:44 +0900

    환경 설정

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("hello");
+import express from "express";
+
+const app = express();
+//설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+//경로 설정
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); //다른 경로 입력해도 /으로 리다이렉트
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen); //포트번호 3000
