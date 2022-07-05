bde6e7ea9de46d026ddac82c2c5a82f8c83431ca
Berom <310o@naver.com>
2022-07-05 15:08:02 +0900

    add : Pug view Engine, Renderer, public route
    Pug 형식의 view 엔진과 렌더러 생성
    유저에게 파일 공유를 하기 위한 정적 경로 설정
    nodemon을 server.js를 수정할 때만 새로고침 되도록 설정

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
   - npm i @babel/core @babel/cli @babel/node -D
 - package.json의 scripts
   - //nodemon 호출되면 nodemon.js를 살펴보고 nodemon 실행
+- nodemon은 sever.js 를 수정할 때만 새로 고침하도록 하고 싶음
--- src/server.js
+++ src/server.js
@@ -6,5 +6,13 @@
 import express from "express";
 
 const app = express();
-console.log("hello");
-app.listen(3000);
+//렌더링할 view 엔진 설정과 그경로
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+// 정적 경로 설정 -> 유저에게 파일 공유
+app.use("public", express.static(__dirname + "public"));
+
+app.get("/", (req, res) => res.render("home")); //home.pug를 렌더링 해주는 renderer
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
