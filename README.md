47dd45e0c61b4277d452941dfff61c83b9e301ee
Berom <310o@naver.com>
2022-07-05 15:41:37 +0900

    add : web socket server http와 web socket의 차이

--- README.md
+++ README.md
@@ -5,3 +5,21 @@
 - package.json의 scripts
   - //nodemon 호출되면 nodemon.js를 살펴보고 nodemon 실행
 - nodemon은 sever.js 를 수정할 때만 새로 고침하도록 하고 싶음
+  - 우리 프로젝트를 살펴보고 서버를 재시작해주는 것임
+  - 우리가 한 파일을 javascript로 컴파일 해준다
+
+- Web socket
+  - 악수하는 것처럼 서버와 유저는 연결된다
+  - 서버는 request를 더 이상 기다리지 않고 유저에게 데이터를 보내고 받을 수 있다
+  - 유저와 서버를 양방향 연결을 한다
+- what is http
+  - http는 모든 서버에서 작동하는 방식이다
+  - 유저가 requset를 보내면 서버가 반응하는 구조
+  - stateless : backend가 유저를 기억하지 못한다
+  - 유저와 backend는 분리 되어 있어 데이터를 주고 받고 백엔드는 유저를 잊어버린다
+  - real time은 아님 - 서버가 능동적으로 데이터를 줄 수도 없고 대기를 해야한다.
+- 서버에게 쿠키를 보내면 서버는 유저에게 쿠키에 알맞은 응답을 하고 잊어버린다
+
+- ws를 사용할 것이다
+  - express는 http이므로 둘의 프로토콜은 다르다
+  - express는 ws를 지원하지 않는다
--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@
 // package.json
 // Babel
 // nodemon
-
+import http from "http";
 import express from "express";
-
+import WebSocket from "ws";
 const app = express();
 //렌더링할 view 엔진 설정과 그경로
 app.set("view engine", "pug");
@@ -13,10 +13,22 @@ app.set("views", __dirname + "/views");
 // 정적 경로 설정 -> 유저에게 파일 공유
 // 유저가 Public 폴더로 이동하면 그 파일을 볼 수 있다
 app.use("public", express.static(__dirname + "public"));
-
 //home.pug를 렌더링 해주는 renderer
 app.get("/", (req, res) => res.render("home"));
 // 유저가 어떤 url로 이동하던 다시 home으로 redirect 해주는 것임
-app.get("/*",(req,res )=> res.redirect("/"));
+app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+//createServer에 등장하는 리퀘스트 핸들러는 app이다!
+// 이제는 app.listen 하기 전에 서버에 접근할 수 있음
+// http가 필요한 이유는 위에 redirect, view 등의 기능이 필요하기 때문이다
+const server = http.createServer(app);
+// 이렇게 하면 http 서버와 socket 서버를 동시에 운영할 수 있다
+// 두 서버를 같은 포트에서 사용하길 원하기 때문이다
+const wss = new WebSocket.Server({ server });
+
+// 내 http 서버에 접근 할 수 있고 http 서버 위에 Web Socket 서버를 만들 수 있음
+// 물론 꼭 이렇게 할 필요가 없긴하다
+server.listen(3000,handleListen);
+
