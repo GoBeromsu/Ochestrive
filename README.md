7ff465be938ceb050eafa7e121b095ac618fe766
Berom <310o@naver.com>
2022-07-05 14:38:59 +0900

    add : setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Ochestrive
+
+- Babel download
+  - npm i @babel/core @babel/cli @babel/node -D
+- package.json의 scripts
+  - //nodemon 호출되면 nodemon.js를 살펴보고 nodemon 실행
---
+++ src/server.js
@@ -0,0 +1,10 @@
+// ExpressJS, app.get(), Pug, (req,res) =>
+// package.json
+// Babel
+// nodemon
+
+import express from "express";
+
+const app = express();
+console.log("hello");
+app.listen(3000);
