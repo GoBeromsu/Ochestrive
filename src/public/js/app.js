228418b908746affd3d0b170b4e44a4163675f36
Berom <310o@naver.com>
2022-07-18 14:20:15 +0900

    Feat : getCameras && handleCameraChange - 현재 선택된 비디오 출력 안되는 문제 해결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,18 +3,53 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getMedia() {
+// 카메라 id를 가져옵니다
+async function getCameras() {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label == camera.label) {
+        option.selected = true;
+      }
+      cameraSelect.appendChild(option);
     });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// device ID : camera ID임
+// data stream 초기화 및 설정
+// stream의 핵심은 track을 제공해준다는 점이다!
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" }, //facing mode로 처음 시작시 어느 스트림부터 보낼지 정해 줄 수 있음
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } }, //exact 썼는데 디바이스 없으면 아예 스트림 출력 안함
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
+
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -48,5 +83,12 @@ function handleCameraClick() {
   }
 }
 
+async function handleCameraChange() {
+  // cameraSelect로 카메라 ID를 가져오면, getMedia에서 비디오 스트림을 전환해준다.
+  await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
+
 cameraBtn.addEventListener("click", handleCameraClick);
+cameraSelect.addEventListener("input", handleCameraChange);
