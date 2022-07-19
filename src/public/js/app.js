ba95cb3275e877711774f30d80ecd2429edfaef3
Berom <310o@naver.com>
2022-07-19 13:49:06 +0900

    Feat : Stun Server

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -158,7 +158,19 @@ socket.on("ice", (ice) => {
 });
 
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addStream", handleAddStream);
   myStream
