3dc20323dadb1e1643ac14feb5444bd6e672ef70
Berom <310o@naver.com>
2022-08-03 22:30:19 +0900

    Feat : TestPage View && User Register

--- FrontEnd/Client.js
+++ FrontEnd/Client.js
@@ -1,15 +1,49 @@
-var socket = io();
-var participants = {};
-var name;
+const socket = io();
+const participants = {};
 
+const Name = document.getElementById("Name");
+const nameForm = Name.querySelector('form');
+const Session = document.getElementById("Session");
+const sessionForm = Session.querySelector("form");
 socket.on('connect', () => {
-    console.log('ws connect success')
+    console.log('Web Socket connection success')
+
 });
 
-socket.on('message', message => {
-    console.log('Recieved message')
+// socket.on('message', message => {
+//     console.log('Recieved message')
 
-    switch (message) {
+//     switch (message) {
 
+//     }
+// })
+
+
+
+// User 등록하는 함수
+async function register(event) {
+    event.preventDefault();
+    const name = nameForm.querySelector("input").value;
+    const session = sessionForm.querySelector("input").value;
+
+    var message = {
+        id: 'register',
+        name: name,
+        session: session
     }
-})
+
+    sendMessage(message);
+}
+
+
+function sendMessage(message) {
+    var jsonMessage = JSON.stringify(message);
+    console.log('Client :  ' + jsonMessage);
+    socket.emit('message', jsonMessage);
+}
+
+
+
+// Btn && EventListener
+
+sessionForm.addEventListener("submit", register);
